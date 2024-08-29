e4239fe434c4902ee79922f4fc9279d3d91303fc
Ryan Montgomery <ryan.montgomery@frayt.com>
2024-08-29 14:19:18 -0500

    init devkit install

---
+++ dev_kit_macos.sh
@@ -0,0 +1,23 @@
+
+brew install --cask docker
+brew install --cask dbeaver-community
+brew install --cask iterm2
+brew install --cask postman
+brew install --cask visual-studio-code
+
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension hashicorp.terraform
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension jakebecker.elixir-ls
+code --install-extension outstand.outstand-elixir-ls
+code --install-extension saratravi.elixir-formatter
+code --install-extension phoenixframework.phoenix
+
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
