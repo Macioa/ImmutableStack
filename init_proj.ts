e880a37347ae2ab56fef7217a9955c5cad1a52c1
Ryan Montgomery <ryan.montgomery@frayt.com>
2024-09-22 16:23:24 -0500

    convert init proj to ts

---
+++ init_proj.ts
@@ -0,0 +1,36 @@
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+  console.error("Usage: node init_proj.js <project_name>");
+  process.exit(1);
+}
+
+let projectName = args[0];
+
+projectName = projectName.toLowerCase().replace(/[\s-]/g, '_').replace(/[^a-z0-9_]/g, '');
+
+const 
+    umbrellaDir = `${projectName}_umbrella`,
+    appdir = path.join(umbrellaDir, 'apps'),
+    uidir = path.join(appdir, `${projectName}_ui`);
+
+console.log("\nGenerating Phoenix apps...");
+execSync(`mix phx.new ${projectName} --no-live --no-html --no-assets --binary-id --umbrella --install`, { stdio: 'inherit' });
+
+// Modify mix.exs to add apps: [projectName, `${projectName}_web`] underneath apps_path
+const mixExsPath = path.join(`${projectName}_umbrella`, 'mix.exs');
+let mixExsContent = fs.readFileSync(mixExsPath, 'utf8');
+mixExsContent = mixExsContent.replace(/apps_path: "apps"/, `apps_path: "apps",\n  apps: [:${projectName}, :${projectName}_web]`);
+fs.writeFileSync(mixExsPath, mixExsContent, 'utf8');
+execSync(`cd ${projectName}_umbrella && mix format mix.exs`, { stdio: 'inherit' });
+
+console.log("\nGenerating React app...");
+execSync(`cd ${appdir} && npx create-react-app ${projectName}_ui --template typescript`, { stdio: 'inherit' });
+console.log("\nInstalling Redux...");
+
+execSync(`cd ${uidir} && npm install @reduxjs/toolkit react-redux @types/react-redux`, { stdio: 'inherit' });
+execSync(`cd ${uidir} && npm install --save-dev @babel/plugin-proposal-private-property-in-object`, { stdio: 'inherit' });
