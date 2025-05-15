06ce5199208cbd048f5eb21b4a51d3490278b2e2
Ryan Montgomery <ryan.montgomery@frayt.com>
2025-05-15 08:42:17 -0500

    rename and reorganize phoenix deps

--- injectors/init_phoenix/inject_phoenix_deps.ts
+++ injectors/init_phoenix/inject_phoenix_deps.ts
@@ -1,56 +1,46 @@
 import path from "path";
-import { inject_file, Injection, InjectType } from "../index";
+import { inject_file, Injection, InjectType as T } from "../index";
 import { AppData } from "../../readers/get_app_data";
 
-const inject_web_app_libs = async (AppNameSnake: string, WebDir: string) => {
+const inject_web_app_deps = async ({ WebDir }: AppData) => {
   const file = path.join(WebDir, "mix.exs");
   const injections: Injection[] = [
     [
-      InjectType.AFTER,
+      T.AFTER,
       /defp\sdeps\sdo\s*\n{0,5}\s*\[/,
-      `\n      {:cors_plug, "~> 2.0"},`,
+      `\n      {:cors_plug, "~> 2.0"},\n`,
     ],
   ];
 
-  return inject_file({ file, injections }, "inject_web_app_libs");
+  return inject_file({ file, injections }, "inject_web_app_deps");
 };
 
-const inject_umbrella_libs = async (
-  AppNameSnake: string,
-  UmbrellaDir: string
-) => Promise.resolve(true);
-
-const inject_app_libs = async (UmbrellaDir: string) => {
+const inject_umbrella_deps = async ({ UmbrellaDir }: AppData) => {
   const file = path.join(UmbrellaDir, "mix.exs");
+  const injections: Injection[] = [];
+
+  return inject_file({ file, injections }, "inject_app_deps");
+};
+
+const inject_app_deps = async ({ LibDir }: AppData) => {
+  const file = path.join(LibDir, "mix.exs");
   const injections: Injection[] = [
     [
-      InjectType.AFTER,
-      /\{\:ecto_sql\, \".*\"\}\,/,
-      `\n      {:scrivener_ecto, "~> 3.0"},`,
+      T.AFTER,
+      /defp\sdeps\sdo\s*\n{0,5}\s*\[/,
+      `\n{:scrivener, "~> 2.7"}, {:scrivener_ecto, "~> 3.0"}, {:phoenix, "~> 1.7.14"}, {:jason, "~> 1.2"}, {:dns_cluster, "~> 0.1.1"}, {:phoenix_pubsub, "~> 2.1"}, \n`,
     ],
   ];
 
-  return inject_file({ file, injections }, "inject_app_libs");
+  return inject_file({ file, injections }, "inject_app_deps");
 };
 
-interface Phx_Dirs {
-  WebDir?: string;
-  AppDir?: string;
-  UmbrellaDir?: string;
-  LibDir?: string;
-}
-
-const inject_phoenix_libs = async ({
-  AppNameSnake,
-  WebDir,
-  UmbrellaDir,
-  LibDir,
-}: AppData) => {
+const inject_phoenix_deps = async (appdata: AppData) => {
   return Promise.all([
-    inject_web_app_libs(AppNameSnake, WebDir || ""),
-    inject_app_libs(LibDir || ""),
-    inject_umbrella_libs(AppNameSnake, UmbrellaDir || ""),
+    inject_web_app_deps(appdata),
+    inject_app_deps(appdata),
+    inject_umbrella_deps(appdata),
   ]);
 };
 
-export { inject_phoenix_libs };
+export { inject_phoenix_deps };
